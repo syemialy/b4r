5f9cb0a10c271cde18bdf76cf10a708fd7d3287c
Sergei Emelianov <semelianov@gmail.com>
2015-01-17 19:49:49 +0300

    added modifications to individual ads

--- b4r-usd.js
+++ b4r-usd.js
@@ -1,37 +1,81 @@
 // Copyright (c) 2015 Sergei Emelianov (seego). 
 // http://facebook.com/sergei.emelianov
 //All rights reserved.
+//open for offers & ideas
+//public github
 
-var byrusd=15310;
-var timeout = null;
-var priceChanged = null;
+var byrusd=null;
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
     // fired when a mutation occurs
-    console.log(mutations, observer);
-    // ...
+    var mutationTarget = null;
+    $.each(mutations, function(index, amutation) {
+    	//this allows me to detect change of content in auto and moto baraholka
+    	mutationTarget = amutation.target.className.toString();
+    	if ( mutationTarget.match(/autoba-count/gi) != null ) {
+    		// console.log( "found: " + amutation );	
+    		changePrices('div.cost-i > p.big > a > strong');
+    	} 
+    });
 });
 
+//When page is loaded, we check what will be the best way to change  prices 
 $(document).ready(function(){
-	observer.observe(document.body, {
-  	subtree: true,
-  	attributes: true
-	});
+	var uri = document.documentURI;
+	if ( uri.match(/[am]b\.onliner\.by\/car|moto\/\d+/g) != null ) {
+		changePrices('span.autoba-hd-details-costs > span.cost > strong');
+		replaceRub('span.autoba-hd-details-costs > span.cost');
+	} else if ( uri.match(/baraholka\.onliner\.by\/viewtopic\.php\?t\=\d+/g)) {
+		replaceRub('ul.b-ba-topicdet > li.cost > small');
+		changePrices('div.m-title > ul.b-ba-topicdet > li.cost');
+	} else if (uri.match(/[am]b\.onliner\.by/g) != null ) {
+		observer.observe(document.body, {
+	  		subtree: true,
+	  		attributes: true
+		});
+	} else if ( uri.match(/baraholka\.onliner\.by/g) != null ) {
+		changePrices('td.cost > big > strong');
+		//remove руб.
+		replaceRub('td.cost > big');
+	}
+	
 });
 
-function priceChange() {
-		console.log('priceChange is fired');
-		var changedPrices = 0;
-  		$('div.cost-i > p.big > a >strong').each(
-      		function(index, priceSection){
-      			changedPrices++;
-        		var priceByr = $(priceSection).text();
-        		priceByr = priceByr.replace(/ /g,'');
-        		$(priceSection).text(parseFloat(Math.round(priceByr/byrusd).toFixed(2)));
-    		});
-  		if (changedPrices > 0) {
-  			priceChanged = 1;
-  		}
+/**
+* replaces руб.
+*/
+function replaceRub(lookup) {
+	$(lookup).each( function(index, elem) {
+			var innerTxt = elem.innerText;
+			innerTxt = innerTxt.replace(/руб\./g, '');
+			elem.innerText = innerTxt;
+		});
+}
+/**
+* replaces price in BYR to USD
+*/
+function changePrices(lookup) {
+	//read current exchange rate
+	if ( !byrusd ) {
+		byrusd = $('li.top-informer-currency > a > span._u').text().replace(/[ \$]/g,'');
+		byrusd = parseFloat(byrusd);
+		// console.log("current rate: 1byr/"+byrusd);
+	}
+
+	$(lookup).each(
+  		function(index, priceSection){
+    		var priceByr = $(priceSection).text();
+    		//checking if the price already changed
+    		if ( priceByr.match(/\$/g) == null ) {
+    			priceByr = priceByr.replace(/ /g,'');
+    			var priceUsd = parseFloat(Math.round(priceByr/byrusd).toFixed(2));
+    			$(priceSection).text("$ "+ numberWithCommas(priceUsd));	
+    		}
+		});
 };
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
